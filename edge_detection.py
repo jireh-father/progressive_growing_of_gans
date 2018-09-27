c0209773d37177abde1a063c00cb1d6d492a6ac8
seoilgun <seoilgun@gmail.com>
2018-09-27 14:24:04 +0900

    test

--- edge_detection.py
+++ edge_detection.py
@@ -6,6 +6,15 @@ output_dir = img_dir + "_edge"
 assert img_dir
 if not os.path.isdir(output_dir):
     os.makedirs(output_dir)
+
+thumb_list = glob.glob(os.path.join(img_dir, '*thumb.jpg'))
+if len(thumb_list) > 0:
+    thumb_path = img_dir + "_thumb"
+    if not os.path.isdir(thumb_path):
+        os.makedirs(thumb_path)
+    for thumb_file in thumb_list:
+        os.rename(thumb_file, os.path.join(thumb_path, os.path.basename(thumb_file)))
+
 imgs = glob.glob(os.path.join(img_dir, "*.jpg"))
 # plt.subplot(121)
 for i, path in enumerate(imgs):
