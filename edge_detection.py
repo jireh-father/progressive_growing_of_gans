5f2073bfa54f83a5f47e8308c945b7c7286dbc70
seoilgun <seoilgun@gmail.com>
2018-10-08 14:23:28 +0900

    test

--- edge_detection.py
+++ edge_detection.py
@@ -3,6 +3,7 @@ import os, glob
 
 img_dir = "/home/data4/dataset/cate_cut_0411/shirt"
 output_dir = img_dir + "_edge"
+use_rgb = True
 assert img_dir
 if not os.path.isdir(output_dir):
     os.makedirs(output_dir)
@@ -20,4 +21,6 @@ imgs = glob.glob(os.path.join(img_dir, "*.jpg"))
 for i, path in enumerate(imgs):
     img = cv2.imread(path, 0)
     edges = cv2.Canny(img, 20, 70)
+    if use_rgb:
+        edges = cv2.cvtColor(edges, cv2.COLOR_GRAY2RGB)
     cv2.imwrite(os.path.join(output_dir, os.path.basename(path)), edges, [int(cv2.IMWRITE_JPEG_QUALITY), 100])
